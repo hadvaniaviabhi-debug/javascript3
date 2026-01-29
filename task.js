87e23a6bd97e9b4e6ea7877369950e6b8a22ee01
hadvaniaviabhi-book <hadvaniaviabhi@gmail.com>
2026-01-29 16:23:10 +0530

    [HA] [task example add]

---
+++ task.js
@@ -0,0 +1,28 @@
+let student ={
+    name: "Hadvani Abhi",
+    email:"HADVANIAVIABHI@GMAIL.COM",
+    course:"js,react,node",
+    marks: [45,78,60,90,33],
+    hobbies: ["music","cricket","reading"],
+    address: ["india",["gujarat","ahemedabad"]]
+}
+
+
+
+//string operation
+
+//remove extra spaces from name and conver it to uppercase
+console.log("remove extra spaces",student.name.trim())
+console.log("convert in to uppercase",student.name.toUpperCase())
+//convert email to lowercase 
+console.log("convert in to toLowerCase",student.email.toLowerCase())
+//check whether course  includes "react"
+console.log("whether course  includes react",student.course.includes("react"));
+//find the index of "node" in course
+console.log("find the index of node in course",student.course.indexOf("node"));
+//replace "javascript" with "js"
+console.log("replace javascript with js",student.course.replace("javascript","js"));
+//convert course string into an array
+console.log("convert course string into an array",student.course.split(","));
+//display the length of student name 
+console.log("display the length of student name ",student.name.length);
