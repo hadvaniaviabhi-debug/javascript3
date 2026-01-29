f313bc4998aa4245a1a222594e9c272952f5a4f0
hadvaniaviabhi-book <hadvaniaviabhi@gmail.com>
2026-01-29 16:51:59 +0530

    [HA] [this is exmaple]

--- task.js
+++ task.js
@@ -14,15 +14,63 @@ let student ={
 //remove extra spaces from name and conver it to uppercase
 console.log("remove extra spaces",student.name.trim())
 console.log("convert in to uppercase",student.name.toUpperCase())
+
+
 //convert email to lowercase 
 console.log("convert in to toLowerCase",student.email.toLowerCase())
+
+
 //check whether course  includes "react"
 console.log("whether course  includes react",student.course.includes("react"));
+
+
 //find the index of "node" in course
 console.log("find the index of node in course",student.course.indexOf("node"));
+
+
 //replace "javascript" with "js"
 console.log("replace javascript with js",student.course.replace("javascript","js"));
+
+
 //convert course string into an array
 console.log("convert course string into an array",student.course.split(","));
+
+
 //display the length of student name 
 console.log("display the length of student name ",student.name.length);
+
+
+//array operation
+
+//add "swimming" to hobbies
+student.hobbies.push("swimming");
+console.log(student.hobbies);
+
+
+//remove the last hobby
+student.hobbies.pop();
+console.log(student.hobbies);
+
+//add "drawing" at the beginning of hobbies
+student.hobbies.unshift("drawing");
+console.log(student.hobbies);
+
+//remove the first hobby 
+student.hobbies.shift();
+console.log(student.hobbies);
+
+
+//merge hobby witih course arry 
+let mergedArray = student.course.concat(student.hobbies);
+console.log(mergedArray);
+
+
+//check if "react" exists in course array
+
+//find index of "js" in course array 
+
+//extract first two marks
+
+//remove one failed subject mark (less than 40) using splice 
+
+//convert hobbies array into  a string 
